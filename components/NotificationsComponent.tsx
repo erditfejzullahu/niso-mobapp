22771569924fbdaa02f4e20498bf0282c23525b6
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-08-11 02:49:31 +0200

    improve ui of notifiations component

--- components/NotificationsComponent.tsx
+++ components/NotificationsComponent.tsx
@@ -1,30 +1,45 @@
 import { useToggleNotifications } from "@/store/useToggleNotifications";
-import { BottomSheetModal, BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { BottomSheetModal, BottomSheetModalProvider, BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import dayjs from "dayjs";
+import "dayjs/locale/sq";
+import relativeTime from "dayjs/plugin/relativeTime";
 import React, { memo, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+dayjs.extend(relativeTime);
+dayjs.locale('sq')
 
 const fakeNotifications = [
-  'Tarifa juaj u aprovua ✅',
-  'Keni një udhëtim të ri 📍',
-  'Pagesa prej €5.00 u pranua 💳',
-  'Përdoruesi ju dha vlerësim 5⭐',
+  {message: "Tarifa juaj u aprovua ✅", isRead: true},
+  {message: "Keni një udhëtim të ri 📍", isRead: false},
+  {message: "Tarifa juaj u aprovua ✅", isRead: true},
+  {message: "Keni një udhëtim të ri 📍", isRead: false},
+  {message: "Tarifa juaj u aprovua ✅", isRead: true},
+  {message: "Pagesa prej €5.00 u pranua 💳", isRead: false},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: true},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: false},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: true},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: false},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: false},
+  {message: "Përdoruesi ju dha vlerësim 5⭐", isRead: true},
 ];
 
 
 const NotificationsComponent = () => {
   const bottomSheetRef = useRef<BottomSheetModal>(null)
     const {isOpened} = useToggleNotifications();
-    console.log(isOpened);
-    
+    // console.log(isOpened);
+    const dateCreated = "2025-08-09T14:22:00Z";
+    const clientPhoto ="https://randomuser.me/api/portraits/men/32.jpg";
+
     if(isOpened) bottomSheetRef.current?.dismiss(); else bottomSheetRef.current?.present();
 
   return (
     <BottomSheetModalProvider>
         <BottomSheetModal
           ref={bottomSheetRef}
-          index={1} // Start expanded
-          snapPoints={["100%", '100%']}
+          index={0} // Start expanded
+          snapPoints={["50%", '80%']}
           enableDynamicSizing={false}
           enablePanDownToClose={true}
           style={styles.bottomSheet}
@@ -35,11 +50,24 @@ const NotificationsComponent = () => {
             />
           )}
         >
-          <View style={styles.bottomSheetContent}>
-            {fakeNotifications.map((item, idx) => (
-                <Text key={idx} className="font-pregular text-sm">{item}</Text>
-            ))}
-          </View>
+            <>
+                <BottomSheetScrollView contentContainerStyle={styles.bottomSheetContent}>
+                    {fakeNotifications.map((item, idx) => (
+                        <TouchableOpacity key={idx} className={`w-full flex-row items-center gap-2 ${item.isRead ? "bg-white" : "bg-indigo-950 animate-pulse"} shadow-lg shadow-black/10 my-1 rounded-lg p-3`}>
+                            <View>
+                                <Image 
+                                    source={{uri: clientPhoto}}
+                                    className="w-12 h-12 rounded-full"
+                                />
+                            </View>
+                            <View className="pb-[14px] flex-1">
+                                <Text className={`font-pregular ${item.isRead ? "text-indigo-950" : "text-white"} text-sm break-words`}>{item.message}</Text>
+                            </View>
+                                <Text className="absolute bottom-1 right-1 text-indigo-950 font-pregular text-xs bg-white rounded-md px-2 py-0.5 shadow-sm shadow-black/20  border-gray-200">{dayjs(dateCreated).fromNow()}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </BottomSheetScrollView>
+            </>
         </BottomSheetModal>
         </BottomSheetModalProvider>
   )
@@ -49,16 +77,21 @@ export default memo(NotificationsComponent)
 
 const styles = StyleSheet.create({
   bottomSheet: {
-    shadowColor: '#000',
+    shadowColor: 'rgba(0,0,0,0.1)',
     shadowOffset: { width: 0, height: -3 },
-    shadowOpacity: 0.1,
+    shadowOpacity: 5,
     shadowRadius: 4,
     elevation: 5,
   },
   bottomSheetContent: {
-    flex: 1,
-    padding: 16,
+    // flex: 1,
+    // padding: 16,
+    paddingBottom: 60,
+    paddingTop: 2,
+    paddingInline: 8,
     alignItems: 'center',
-    backgroundColor: '#f9f9f9',
+    backgroundColor: '#f9fafb',
+    borderTopColor: "rgba(0,0,0,0.05)",
+    borderTopWidth: 1,
   },
 })
