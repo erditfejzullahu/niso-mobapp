dd007ab06505b5ad440cb63b31df8ed380fcc240
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-08-17 01:07:22 +0200

    initilzie firebase

--- app/(root)/client/section/create-transport.tsx
+++ app/(root)/client/section/create-transport.tsx
@@ -1,12 +1,89 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import HeaderComponent from '@/components/HeaderComponent';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 const CreateTransport = () => {
-  return (
-    <View>
-      <Text>CreateTransport</Text>
-    </View>
-  )
-}
 
-export default CreateTransport
+  const driversActive = useSharedValue(1);
+  
+  React.useEffect(() => {
+    driversActive.value = withRepeat(
+      withTiming(1.2, {
+        duration: 1500,
+        easing: Easing.inOut(Easing.ease)
+      }),
+      -1,
+      true
+    )
+  }, [])
+
+  const driversActiveStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: driversActive.value}]
+    }
+  })
+
+  const videoSource = require('../../../../assets/videos/driver.mp4');
+
+  const player = useVideoPlayer(videoSource, player => {
+    player.play();
+    player.loop = true;
+  });
+
+  useEffect(() => {
+    if(player){
+      player.play();
+      player.loop = true;
+    }
+  }, [player])
+  
+  
+
+  // useFocusEffect(
+  //   useCallback(() => {
+  //     return () => {
+  //       if(player){
+  //         player.pause();
+  //         player.release();
+  //       }
+  //     };
+  //   }, [player])
+  // );
+
+  return (
+    <KeyboardAwareScrollView className="bg-gray-50 p-4">
+      <HeaderComponent 
+        title="Kërko transport"
+        subtitle="Duke plotësuar fushat e mëposhtme ju mund të kërkoni transportin më të shpejtë dhe më të lirë ne vend!"
+      />
+      <View className='w-full rounded-xl shadow-md shadow-black/10 mt-3'>
+        <View className='flex-row items-center self-start overflow-hidden mx-auto justify-center gap-1 bg-white shadow-md shadow-black/10 rounded-xl'>
+          <VideoView 
+            player={player} 
+            style={{ width: 36, height: 36 }} 
+          />
+          
+          {/* ✅ Animated.Text wrapping the number */}
+          <Animated.Text style={driversActiveStyle} className="font-pbold text-indigo-600 text-sm">
+            10
+          </Animated.Text>
+          <Animated.Text className="font-psemibold text-sm text-indigo-950">
+            {' '}shofer aktiv afër jush!
+          </Animated.Text>
+          <VideoView 
+            player={player} 
+            style={{ width: 36, height: 36 }} 
+          />
+        </View>
+      </View>
+
+      
+
+    </KeyboardAwareScrollView>
+  );
+};
+
+export default CreateTransport;
---
+++ firebase.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth: Auth = getAuth(app)
+const db: Firestore = getFirestore(app)
+const analytics = getAnalytics(app);
+
+export { analytics, auth, db };
