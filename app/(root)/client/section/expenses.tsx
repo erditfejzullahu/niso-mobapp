799c691ab26a49203830d7aa561ba81665d25edd
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-09-10 18:33:36 +0200

    update

--- app/(root)/client/section/expenses.tsx
+++ app/(root)/client/section/expenses.tsx
@@ -9,7 +9,7 @@ import { PassengerAllExpensesList, PassengerFinances, PassengerRecentSpents } fr
 import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { ChartColumn, CheckCircle, Clock, CreditCard, Euro, Receipt, X } from "lucide-react-native";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { FlatList, Modal, RefreshControl, Text, TouchableOpacity, View } from "react-native";
 import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated";
 
@@ -52,6 +52,86 @@ export default function ClientExpenses() {
   
   console.log(allFinancesData);
   
+  const headComp = useMemo(() => (
+    <>
+      <View className="mb-4">
+        <HeaderComponent
+          title="Financat e tua" 
+          style={'!bg-transparent'} 
+          subtitle={"Ketu mund te percillni gjendjen tuaj te financave. Kur keni paguar per udhetim, sa, detaje udhetimi, e shume opsione tjera."}
+        />
+      </View>
+      <View className="mb-4 gap-4">
+        {/* Total Spent */}
+        <View className="bg-white rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50">
+          <Euro size={28} color="#4338ca" />
+          <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
+            {data?.totalSpent || 0} €
+          </Text>
+          <Text className="text-gray-500 mt-1 font-pmedium">
+            Shpenzimet totale
+          </Text>
+        </View>
+
+        {/* Completed Rides */}
+        <View className="flex-row justify-between">
+          <View className="bg-white flex-1 rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50 mr-2">
+            <CheckCircle size={28} color="#4338ca" />
+            <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
+              {data?.completedDrives || 0}
+            </Text>
+            <Text className="text-gray-500 mt-1 font-pmedium">
+              Udhëtime të kryera
+            </Text>
+          </View>
+          <View className="bg-white flex-1 rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50 ml-2">
+            <Receipt size={28} color="#4338ca" />
+            <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
+              {data?.refundedPayments || 0} €
+            </Text>
+            <Text className="text-gray-500 mt-1 font-pmedium">
+              Pagesa të kthyera
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      {/* Average + Pending */}
+      <View className="flex-row justify-between mb-6">
+        <View className="flex-1 bg-white rounded-2xl p-5 shadow-md shadow-black/5 mr-2 border border-gray-50">
+          <ChartColumn size={28} color="#4338ca" />
+          <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
+            {data?.averagePerDrive || 0} €
+          </Text>
+          <Text className="text-gray-500 mt-1 font-pmedium">
+            Mesatarisht për udhëtim
+          </Text>
+        </View>
+
+        <View className="flex-1 bg-white rounded-2xl p-5 shadow-md shadow-black/5 ml-2 border border-gray-50">
+          <Clock size={28} color="#4338ca" />
+          <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
+            {data?.pendingPayments || 0} €
+          </Text>
+          <Text className="text-gray-500 mt-1 font-pmedium">
+            Pagesa në pritje
+          </Text>
+        </View>
+      </View>
+
+      {/* Recent Payments */}
+      <View className="flex-row justify-between items-center mb-3">
+        <Text className="text-indigo-900 font-psemibold text-xl">
+          Pagesat e fundit
+        </Text>
+        <Animated.View style={pulseStyle}>
+          <TouchableOpacity onPress={() => setOpenedAllFinancesModal(true)} className="shadow-lg shadow-black/5 bg-white p-1 rounded-lg">
+            <CreditCard color={"#312e81"} />
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
+    </>
+  ), [data?.averagePerDrive, data?.pendingPayments, data?.refundedPayments, data?.completedDrives, data?.totalSpent])
 
   if(isLoading || isRefetching) return (<View className="h-full bg-gray-50"><LoadingState /></View>);
   if(error) return (<View className="h-full bg-gray-50"><ErrorState onRetry={refetch}/></View>)
@@ -86,86 +166,7 @@ export default function ClientExpenses() {
             </Text>
           </View>
         )}
-        ListHeaderComponent={() => (
-          <>
-            <View className="mb-4">
-              <HeaderComponent
-                title="Financat e tua" 
-                style={'!bg-transparent'} 
-                subtitle={"Ketu mund te percillni gjendjen tuaj te financave. Kur keni paguar per udhetim, sa, detaje udhetimi, e shume opsione tjera."}
-              />
-            </View>
-            <View className="mb-4 gap-4">
-              {/* Total Spent */}
-              <View className="bg-white rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50">
-                <Euro size={28} color="#4338ca" />
-                <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
-                  {data?.totalSpent || 0} €
-                </Text>
-                <Text className="text-gray-500 mt-1 font-pmedium">
-                  Shpenzimet totale
-                </Text>
-              </View>
-
-              {/* Completed Rides */}
-              <View className="flex-row justify-between">
-                <View className="bg-white flex-1 rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50 mr-2">
-                  <CheckCircle size={28} color="#4338ca" />
-                  <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
-                    {data?.completedDrives || 0}
-                  </Text>
-                  <Text className="text-gray-500 mt-1 font-pmedium">
-                    Udhëtime të kryera
-                  </Text>
-                </View>
-                <View className="bg-white flex-1 rounded-2xl p-5 shadow-md shadow-black/5 border border-gray-50 ml-2">
-                  <Receipt size={28} color="#4338ca" />
-                  <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
-                    {data?.refundedPayments || 0} €
-                  </Text>
-                  <Text className="text-gray-500 mt-1 font-pmedium">
-                    Pagesa të kthyera
-                  </Text>
-                </View>
-              </View>
-            </View>
-
-            {/* Average + Pending */}
-            <View className="flex-row justify-between mb-6">
-              <View className="flex-1 bg-white rounded-2xl p-5 shadow-md shadow-black/5 mr-2 border border-gray-50">
-                <ChartColumn size={28} color="#4338ca" />
-                <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
-                  {data?.averagePerDrive || 0} €
-                </Text>
-                <Text className="text-gray-500 mt-1 font-pmedium">
-                  Mesatarisht për udhëtim
-                </Text>
-              </View>
-
-              <View className="flex-1 bg-white rounded-2xl p-5 shadow-md shadow-black/5 ml-2 border border-gray-50">
-                <Clock size={28} color="#4338ca" />
-                <Text className="text-3xl font-psemibold mt-3 text-indigo-900">
-                  {data?.pendingPayments || 0} €
-                </Text>
-                <Text className="text-gray-500 mt-1 font-pmedium">
-                  Pagesa në pritje
-                </Text>
-              </View>
-            </View>
-
-            {/* Recent Payments */}
-            <View className="flex-row justify-between items-center mb-3">
-              <Text className="text-indigo-900 font-psemibold text-xl">
-                Pagesat e fundit
-              </Text>
-              <Animated.View style={pulseStyle}>
-                <TouchableOpacity onPress={() => setOpenedAllFinancesModal(true)} className="shadow-lg shadow-black/5 bg-white p-1 rounded-lg">
-                  <CreditCard color={"#312e81"} />
-                </TouchableOpacity>
-              </Animated.View>
-            </View>
-          </>
-        )}
+        ListHeaderComponent={headComp}
         ListEmptyComponent={() => (
           <View className="bg-gray-50 my-4">
             <EmptyState textStyle='!font-plight !text-sm' message="Nuk ka statistika financore ende. Nese mendoni qe eshte gabim ju lutem provoni perseri." onRetry={refetch}/>
